c42683da95cd5b54bd9a43d9fb778e0a50814446
icook <icook@LAPTOP-NCUV50HH>
2025-05-05 18:47:38 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Assignment_5._1._3
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = { 1, 2, 3, 4, 5 };
+
+        }
+
+        public static bool CheckNums(int[] nums)
+        {
+            foreach (int i in nums)
+            {
+                while (i > nums.Length)
+                {
+                    if (i == i + 1)
+                    {
+                        return true;
+                    }
+
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+        }
+    }
+}
